2bd88968dc28223aa192fc5affa836ca697af2b9
gu-ss-tavo <gusstavo.jarabs@gmail.com>
2021-08-29 23:54:31 -0500

    design home view

--- src/scripts/HomeView.js
+++ src/scripts/HomeView.js
@@ -1,5 +1,16 @@
+/**
+ * @author Gusstavo
+ * @description Create home view
+ * 
+ * @param { HTMLElement } context 
+ * @param { function } startGame 
+ * @returns { object }
+ */
 const HomeView = (context, startGame) => {
-    const difficulties = ['ease', 'medium', 'hard'];
+    /**
+     * @description Constant names and values
+     */
+    const difficulties = ['easy', 'medium', 'hard'];
     const categories = {
         mythology: 20,
         sports: 21,
@@ -10,26 +21,38 @@ const HomeView = (context, startGame) => {
     };
     const game_modes = ['random', 'short time', 'survive'];
 
-    // final data
+    /**
+     * @description The 'final' variables are the basis of the structure of the game.
+     */
     let final_difficulty = 'medium';
-
+    //
     let final_categories = {}
     for(let category in categories) {
         final_categories[categories[category]] = false;
     }
-
+    //
     let final_game_mode = 'random';
 
-    // utils
+    /**
+     * @description Utility group for radio type
+     */
     const toggle_difficulties = [];
     const toggle_game_modes = [];
 
-    // events
+    /**
+     * @description Action when submitting form
+     * @param { event } e 
+     */
     const handleSendForm = e => {
         e.preventDefault();
         console.log(final_difficulty, final_categories, final_game_mode);
         startGame();
     }
+
+    /**
+     * @description Action to select a sub-element 'difficulty'
+     * @param { event } e 
+     */
     const handleEntryDifficulty = e => {
         final_difficulty = e.target.getAttribute('value');
         
@@ -41,10 +64,20 @@ const HomeView = (context, startGame) => {
             }
         }
     }
+
+    /**
+     * @description Action to select a sub-element 'categories'
+     * @param { event } e 
+     */
     const handleEntryCategory = e => {
         let value = e.target.getAttribute('value');
         final_categories[value] = e.target.classList.toggle('active');
     }
+
+    /**
+     * @description Action to select a sub-element 'game mode'
+     * @param { event } e
+     */
     const handleEntryGameMode = e => {
         final_game_mode = e.target.getAttribute('value');
 
@@ -57,59 +90,128 @@ const HomeView = (context, startGame) => {
         }
     }
 
-    // create elements
+    /**
+     * @description Creates radio elements, such as { difficulty, game mode }
+     * @param {string} name 
+     * @param {string} value 
+     * @returns {HTMLElement}
+     */
     const createRadioInput = (name, value) => {
+        let container = document.createElement('div');
+        container.classList.add('row', 'cen-x');
+
         let entry = document.createElement('div');
         entry.setAttribute('value', value);
-        entry.innerText = value;    
 
         if(name === 'difficulty') {
+            if(value === 'easy') entry.classList.add('icon-f_easy');
+            else if(value === 'medium') entry.classList.add('icon-f_normal');
+            else if(value === 'hard') entry.classList.add('icon-f_hard');
+
             toggle_difficulties.push(entry);
             entry.onclick = handleEntryDifficulty;
             if(value === final_difficulty) entry.click();
+
+            container.appendChild(entry);
         }else if(name === 'game_mode') {
+            entry.innerText = value;
             toggle_game_modes.push(entry);
             entry.onclick = handleEntryGameMode;
             if(value === final_game_mode) entry.click();
+
+            container.appendChild(entry);
         }
         
-        return entry;
+        return container;
     }
 
-    const createCategoryCheckbox = category =>{
+    /**
+     * @description Creates checkbox items, such as { categories }
+     * @param { string } category 
+     * @returns { HTMLElement }
+     */
+    const createCheckboxEntry = (name, value) =>{
+        let container = document.createElement('div');
+        container.classList.add('row', 'cen-x');
+
         let entry = document.createElement('div');
-        entry.setAttribute('value', categories[category]);
-        entry.innerText = category;
-        entry.onclick = handleEntryCategory;
-        return entry;
+        entry.setAttribute('value', categories[value]);
+
+        if(name === 'category') {
+            entry.classList.add(`icon-f_${value}`);
+            entry.onclick = handleEntryCategory;
+
+            container.appendChild(entry);
+        }
+        return container;
     }
 
-    // structure
+    /**
+     * @description Creates and designs the form
+     */
     let form = document.createElement('form');
+    form.classList.add('form-container', 'col-11', 'row', 'cen-x');
     form.onsubmit = handleSendForm;
 
+    /**
+     * @description Create and design item sections
+     */
+    let difficulties_section = document.createElement('div');
+    let categories_section = document.createElement('div');
+    let game_mode_section = document.createElement('div');
+    difficulties_section.classList.add('difficulty-section', 'col-11', 'row', 'space-around');
+    categories_section.classList.add('category-section', 'col-12', 'row', 'cen-x');
+    game_mode_section.classList.add('game-mode-section', 'col-11', 'row', 'space-around');
+
+    /**
+     * @description Create, design and add 'difficulty' elements
+     */
     difficulties.forEach(difficulty => {
         let element = createRadioInput('difficulty', difficulty);
-        form.appendChild(element);
+        element.classList.add('input-container', 'col-3');
+        difficulties_section.appendChild(element);
     });
 
+    /**
+     * @description Create, design and add 'categories' elements
+     */
     for(let category in categories) {
-        let element = createCategoryCheckbox(category);
-        form.appendChild(element);
+        let element = createCheckboxEntry('category', category);
+        element.classList.add('col-4');
+        categories_section.appendChild(element);
     }
 
+    /**
+     * @description Create, design and add 'game mode' elements
+     */
     game_modes.forEach(game_mode => {
         let element = createRadioInput('game_mode', game_mode);
-        form.appendChild(element);
+        element.classList.add('game-mode', 'col-3');
+        game_mode_section.appendChild(element);
     });
 
+    /**
+     * @description adding sections
+     */
+    form.appendChild(difficulties_section);
+    form.appendChild(categories_section);
+    form.appendChild(game_mode_section);
+
+    /**
+     * @description Create, design and add 'submit' elements
+     */
     let send_form = document.createElement('input');
     send_form.setAttribute('type', 'submit');
-
+    send_form.setAttribute('value', 'Jugar');
+    send_form.classList.add('col-12');
     form.appendChild(send_form);
 
+    /**
+     * @description Render views
+     */
     context.appendChild(form);
 
+    
     return {
         element: form,
     };
